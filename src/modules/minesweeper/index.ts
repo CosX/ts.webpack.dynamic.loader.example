3f88e06ffb8e3e5d156e95eab90a8770f8dc88f8
Karl Solgård <karl.f91@gmail.com>
2020-01-04 00:08:35 +0100

    Oneliner

--- src/modules/minesweeper/index.ts
+++ src/modules/minesweeper/index.ts
@@ -1,8 +1,6 @@
 import minesweeper from './minesweeper.svelte';
 import { IModule } from '../../interfaces/module';
 const app: IModule = {
-    init: (el: HTMLElement, options: Object = {}) => {
-        new minesweeper({ target: el });
-    }
+    init: (el: HTMLElement, options: Object = {}) => new minesweeper({ target: el })
 }
 export default app;
